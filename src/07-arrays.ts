71918b2a910fb0f5a11b03d4e9101e284796abb8
castanedaLuis <castanedaLuis@github.com>
2023-02-15 21:41:35 -0600

    [ 📦 ] Arrays

---
+++ src/07-arrays.ts
@@ -0,0 +1,21 @@
+(() =>{
+    let prices = [1,2,2,1,1,212, 'hola', true];
+    // prices.push('asas');
+    // prices.push(true);
+    // prices.push({});
+    prices.push(121212);
+  
+    let products = ['hola', true];
+    products.push(false);
+  
+    let mixed: (number | string | boolean | Object)[] = ['hola', true];
+    mixed.push(12);
+    mixed.push('as');
+    mixed.push(true);
+    mixed.push({});
+    mixed.push([]); //array es un objeto en javascript
+  
+    let numbers = [1,2];
+    numbers.map(item => item * 2);
+
+})();

fd77392556b24be26511284518a2bb8d52b51b71
castanedaLuis <castanedaLuis@github.com>
2023-02-15 21:09:03 -0600

    [ 📝 ] Tipos inferidos

---
+++ src/03-typing.ts
@@ -0,0 +1,16 @@
+
+(()=>{
+    //motor de inferencia
+    let myProudctName = 'Product 1 '
+    let myProudctPrice = 50
+    
+    
+    myProudctName.toLowerCase();
+    myProudctPrice.toFixed();
+    
+    const stock = 100;
+
+})();
+
+
+
